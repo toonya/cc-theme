2a7568e6d4abe35a9f02a5c40806f59938e52f5b
toonya <msc5762@me.com>
2014-11-29 11:11:53 +0800

    preview 1.2

--- single-portfolio.php
+++ single-portfolio.php
@@ -18,7 +18,7 @@
 		
 			<div class="page-header">
 							
-				<h1 class="page-title container"><?php _e('Portfolio','junkie'); ?></h1>
+				<h1 class="page-title container"><?php the_title(); ?></h1>
 							
 			</div><!-- .page-header -->
 		
@@ -28,12 +28,12 @@
 	        
 			    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			    
-			    	<div class="entry-header">
+			    	<!-- <div class="entry-header">
 		    			<h1 class="widget-title"><?php the_title(); ?></h1>
 						<p class="entry-desc">
 							<?php echo $short_desc; ?>
-						</p><!-- .entry-desc -->
-			    	</div><!-- .entry-header -->
+						</p>
+			    	</div> --><!-- .entry-header -->
 					
 			    	<?php if($video_embed == '') : ?>
 					<?php
@@ -42,7 +42,7 @@
 		                if( $meta ) {
 		                    $field['std'] = __('Edit Gallery', 'junkie');
 		                    $thumbs = explode(',',rtrim($meta, ','));
-		                     $thumbs_output = "<div class='flexslider'><ul class='slides'>";
+		                    $thumbs_output = "<div class='flexslider'><ul class='slides'>";
 		                    foreach( $thumbs as $thumb ) {
 		                        $thumbs_output .= '<li>' . wp_get_attachment_image( $thumb,"large" ) . '</li>';
 		                    }
@@ -69,12 +69,23 @@
 
 	        <div class="entry-meta">
 	        <span class="date">
-				<strong><?php _e('Date', 'junkie'); ?>: </strong>			
-	        	<?php the_time('M Y'); ?>
+				<strong><?php the_date(); ?></strong>			
 	        </span><!-- .date -->
 	        <?php if($client != null) : ?>
 		        <span class="client">
-					<strong><?php _e('Client', 'junkie'); ?>: </strong>			
+					<strong>
+					<?php switch (pll_current_language()) {
+						case 'en':
+							echo 'shake_error_codes';
+							// echo wp_trim_words( get_the_content(), 20 );
+							break;
+						
+						default:
+							echo '分享者';
+							// echo wp_trim_words( get_the_content(), 50 );
+							break;
+					}?>
+					: </strong>			
 		        	<?php echo $client; ?>
 		        </span><!-- .client -->
 	        <?php endif; ?>      
@@ -88,11 +99,6 @@
 					<div class="clear"></div>
 			<?php } ?>
 	        </span><!-- .skills -->
-	        <span class="link">	        
-				<?php if($link != null) : ?>
-					<a target="_blank" href="<?php echo $link; ?>"><?php _e('Launch Project', 'junkie'); ?></a>
-				<?php endif; ?>
-	        </span><!-- .link -->	
 								
 			</div><!-- .entry-meta -->
 			
@@ -103,56 +109,7 @@
 			
 	        <div class="clear"></div>
 	
-		<div id="related-work">
-			<h3 class="section-title"><?php _e('Related Work','junkie'); ?></h3>
-				<ul class="clearfix">	
-				<?php 
-
-				//Set the starter count
-				$start = 4;
-				//Set the finish count
-				$finish = 1;
-								
-				$postId = get_the_ID();						
-				
-                $related = get_posts_related_by_taxonomy($postId, 'portfolio-type', get_the_ID(), array( 'showposts' => 4 ));
-				
-				//Get the total amount of posts
-				$post_count = $related->post_count;
-				
-				
-                while ($related->have_posts()) : $related->the_post(); 
-				
-                ?>
-                
-					<?php if(is_multiple($start, 3)) : /* if the start count is a multiple of 3 */ ?>
-
-                    <?php endif; ?>
-                    
-                    <?php
-                   		$video_embed = get_post_meta(get_the_ID(), 'tj_video_embed_portfolio', TRUE);
-                   		$short_desc = get_post_meta(get_the_ID(), 'tj_portfolio_short_desc', TRUE);
-                    ?>
-                    
-						<li class="portfolio-item <?php if($finish%4==0) { echo "item-last"; } ?>">
-	                        <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">
-	                            <?php the_post_thumbnail('portfolio-thumb', array('class' => 'entry-thumb')); ?>
-	                        </a>
-							<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-							<p class="entry-desc">
-								<?php echo $short_desc; ?>
-							</p><!-- .entry-desc -->				
-						</li><!-- .portfolio-item -->
-
-                    <?php if(is_multiple($finish, 4) || $post_count == $finish) : /* if the finish count is a multiple of 4 or equals the total posts */  ?>
-                    <?php endif; ?>
-                <?php
-				$start++;
-				$finish++;
-				?>
-                <?php endwhile; wp_reset_query(); ?>
-				  </ul><!-- .section-content -->
-       </div><!-- #related-work -->
+	
        
 			</div><!-- #content -->
        
