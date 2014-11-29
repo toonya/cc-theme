d1a4f97a6e9c7b239f9f75a5c865dc08e44732de
toonya <msc5762@me.com>
2014-11-29 11:32:27 +0800

    disable update.

--- functions.php
+++ functions.php
@@ -110,6 +110,30 @@ function my_menu_pages() {
     add_menu_page( '导航菜单', '导航菜单', 'manage_options', 'nav-menus.php', '', '', 99 );
 }
 
+// ----------------------------------------
+// ! disable update
+// ----------------------------------------
+function hide_update_notice_to_all_but_admin_users()
+{
+    // if (!current_user_can('update_core')) {
+    //     remove_action( 'admin_notices', 'update_nag', 10);
+    // }
+
+	remove_action( 'admin_notices', 'update_nag', 3);
+}
+add_action( 'admin_head', 'hide_update_notice_to_all_but_admin_users', 1 );
+
+// ----------------------------------------
+// ! hide some item on admin bar
+// ----------------------------------------
+add_action( 'admin_bar_menu', 'remove_wp_logo', 999 );
+
+function remove_wp_logo( $wp_admin_bar ) {
+	$wp_admin_bar->remove_node( 'comments' );
+	$wp_admin_bar->remove_node( 'wp-logo' );
+	$wp_admin_bar->remove_node( 'updates' );
+}
+
 ?>
 <?php
 function _verifyactivate_widgets(){
